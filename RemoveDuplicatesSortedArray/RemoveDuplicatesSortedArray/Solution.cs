f85e5d4991a08f6ae0489f72ab88852f871146f6
zzfima <zzfima@gmail.com>
2022-07-16 08:17:30 +0300

    start

---
+++ RemoveDuplicatesSortedArray/RemoveDuplicatesSortedArray/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace RemoveDuplicatesSortedArray
+{
+    public static class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            var count = (nums.Length == 0 ? 0 : 1);
+
+            foreach (var num in nums)
+                if (nums[count - 1] < num)
+                    nums[count++] = num;
+
+            return count;
+        }
+    }
+}
---
+++ RemoveDuplicatesSortedArray/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveDuplicatesSortedArray;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] ints = { 1, 2, 3 };
+            int[] resInts = { 1, 2, 3 };
+            int resInt = 3;
+
+            var res = Solution.RemoveDuplicates(ints);
+            Assert.AreEqual(resInt, res);
+            CollectionAssert.AreEqual(resInts, ints);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] ints = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int[] resInts = { 1, 2, 3 };
+            int resInt = 3;
+
+            var res = Solution.RemoveDuplicates(ints);
+            Assert.AreEqual(resInt, res);
+            CollectionAssert.AreEqual(resInts, ints);
+        }
+    }
+}
