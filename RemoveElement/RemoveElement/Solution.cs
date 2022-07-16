5270962ad3c292fd455c654725e039f4d2a02db5
zzfima <zzfima@gmail.com>
2022-07-16 09:39:45 +0300

    start

---
+++ RemoveElement/RemoveElement/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveElement
+{
+    public static class Solution
+    {
+        public static int RemoveElement(int[] nums, int valToRemove)
+        {
+            int count = 0;
+            if (nums.Length == 1)
+                return count;
+            int tail = nums.Length - 1;
+            for (int i = 0; i <= tail; i++)
+            {
+                if (nums[i] == valToRemove)
+                {
+                    nums[i] = nums[tail];
+                    tail--;
+                    i--;
+                }
+                else
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ RemoveElement/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveElement;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] ints = { 0, 1, 2, 2, 3, 0, 4, 2 };
+            int[] resInts = { 0, 1, 4, 0, 3, 0, 4, 2 };
+            int resInt = 5;
+
+            var res = Solution.RemoveElement(ints, 2);
+            Assert.AreEqual(resInt, res);
+            CollectionAssert.AreEqual(resInts, ints);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] ints = { 3, 2, 2, 3 };
+            int[] resInts = { 2, 2, 2, 3 };
+            int resInt = 2;
+
+            var res = Solution.RemoveElement(ints, 3);
+            Assert.AreEqual(resInt, res);
+            CollectionAssert.AreEqual(resInts, ints);
+        }
+    }
+}
